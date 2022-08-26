e505dee46c1152f21e26af1d6a583bf9d13812c9
Marmax73 <almafuertebravo@hotmail.com>
2022-08-26 14:54:28 -0300

    referencia github, completada.

--- scripts/tecnologias/parrafoTecnologia.js
+++ scripts/tecnologias/parrafoTecnologia.js
@@ -7,5 +7,5 @@
 //Párrafo para Tecnologías
 const parrafoTecnologia = document.querySelector('.parrafoTecnologia');
 const parrafo3 = document.createElement('p');
-parrafo3.innerHTML = "<ul><li>html</li><li>css</li><li>javascript</li><li>bootstrap</li><li>scss</li><li>reactjs</li><li>git</li></ul>"
+parrafo3.innerHTML = "<ul><li>html</li><li>css</li><li>javascript</li><li>bootstrap</li><li>scss</li><li>reactjs</li><li>github</li></ul>"
 parrafoTecnologia.appendChild(parrafo3);
