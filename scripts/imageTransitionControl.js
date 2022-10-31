a171e7493f69649315348b654932a2eae415fe0e
Marmax73 <almafuertebravo@hotmail.com>
2022-10-31 20:08:27 -0300

    imgControl script

---
+++ scripts/imageTransitionControl.js
@@ -0,0 +1,11 @@
+const imgCertificado = document.querySelector('.platzi-imagenes')
+
+const imgControl = () =>{
+    if(imgCertificado.classList[a[img[hover]]]){
+        imgCertificado.classList.add('imagenCertificado')
+    }else{
+        imgCertificado.classList.remove('imagenCertificado')
+    }
+}
+
+imgControl()
